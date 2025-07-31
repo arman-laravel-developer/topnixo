76a6cc951da0563ba8c91263b658f3fea4e340ea
Md Arman Ali <mdarmanali070@gmail.com>
2025-07-31 16:00:23 +0600

    Update master.blade.php

--- resources/views/frontend/v-2/master.blade.php
+++ resources/views/frontend/v-2/master.blade.php
@@ -7,6 +7,7 @@
     <title>@yield('title') | {{env('APP_NAME')}}</title>
     <link rel="icon" href="{{asset('setting/'.$setting->logo)}}"/>
     <meta name="facebook-domain-verification" content="9kolwgirfghfrun5zxq2lkqsrvijtp"/>
+    
     <!-- Pavicon ICon -->
     @include('frontend.v-2.includes.style')
 <!-- Google Tag Manager -->
